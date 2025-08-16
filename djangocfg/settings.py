b19d256db38b7216a0c19787f50386dbfe5129ab
Oscar Telleria <theomjty@gmail.com>
2025-08-16 12:02:02 -0400

    static

--- djangocfg/settings.py
+++ djangocfg/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'comunicate_event/static'),  # Ruta exacta a tus estáticos
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
